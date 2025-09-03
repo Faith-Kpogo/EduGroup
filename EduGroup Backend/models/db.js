d0ca88d13d227f7b52740d48aa2e3b8383aa31c1
xorsekpogo@gmail.com <FaithKpogo@gmail.com>
2025-09-03 01:59:54 +0100

    Setor

--- EduGroup Backend/models/db.js
+++ EduGroup Backend/models/db.js
@@ -7,7 +7,9 @@ const db = mysql.createConnection({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
-  ssl: { rejectUnauthorized: true }
+   ssl: {
+    rejectUnauthorized: false 
+  }
 });
 
 db.connect((err) => {
